74218cc8152bab26dee491a4473d0c148c1ff8bf
JcbBrtn <42615937+JcbBrtn@users.noreply.github.com>
2021-07-19 09:23:50 -0400

    Initial Commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,50 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/jcbbrtn/BadSushi/pkg/config"
+	"github.com/jcbbrtn/BadSushi/pkg/models"
+	"github.com/jcbbrtn/BadSushi/pkg/render"
+)
+
+var Repo *Repository
+
+//Repository is the type
+type Repository struct {
+	App *config.AppConfig
+}
+
+//NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handlering
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+
+	stringMap := make(map[string]string)
+	stringMap["test"] = "You've been Chazzzed"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
